0a38092c849bf763a44328bc64b637839bd9c972
sahil saini <sahil.saini@educateapp.in>
2024-10-07 16:49:34 +0530

    random commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     // hello
     console.log("env", process.env.REACT_APP_KEY);
-    console.log("env azure", process.env.AZURE_TEST_FOO);
+    console.log("env azure", process.env.REACT_APP_AZURE_TEST_FOO);
   return (
     <div className="App">
         <p>Hello  {process.env.REACT_APP_KEY}</p>
