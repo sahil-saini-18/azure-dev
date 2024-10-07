2bcb8821f6be92bbc43db79f8117c616adc31f1c
sahil saini <sahil.saini@educateapp.in>
2024-10-07 15:36:22 +0530

    random commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
     // hello
+    console.log("env", process.env.REACT_APP_KEY)
   return (
     <div className="App">
         <p>Hello  {process.env.REACT_APP_KEY}</p>
